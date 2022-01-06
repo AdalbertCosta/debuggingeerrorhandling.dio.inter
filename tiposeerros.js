3b149d9f445a8b5931c1e0b9866189a58e82386a
AdalbertCosta <contato@adalbertocosta.com>
2022-01-06 06:44:55 -0300

    conclusão

---
+++ tiposeerros.js
@@ -0,0 +1,29 @@
+// Tipos e Erros
+
+//ECMAScript Error e DOMException
+
+/* 
+Erros que ocorrem em tempo de execução.
+
+Composto por:
+
+- Mensagem
+- Nome
+- Linha
+Call Stack
+
+*/
+
+let sum = a + 2
+
+// > Uncaught ReferenceError: a is not definede at <anonymous>:1:11
+
+// DOMException
+
+/* 
+
+Erros relacionados ao Document Object Model(DOM)
+
+Erros referentes a estrutura da sua arvore de elementos de sua página WEB.
+
+*/
