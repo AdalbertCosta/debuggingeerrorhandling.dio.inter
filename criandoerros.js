34a92e21e8ae536d4db6b6b231ab64dbcf19b628
AdalbertCosta <contato@adalbertocosta.com>
2022-01-08 14:50:15 -0300

    conclusão

---
+++ criandoerros.js
@@ -0,0 +1,34 @@
+//Criando erros
+//
+//O objeto eror
+
+
+//Código:
+
+
+new Error(message, filename, linNumber)
+
+// todos os parametros são opcionais
+
+const Meu Erro = new Error('Mensagem Inválida');
+
+throw MeuErro;
+
+/*------------------------------*/
+
+const MeuErro = new Error('Mensagem Inválida');
+MeuErro.name = 'InvalidMessage';
+
+throw MeuErro;
+
+/*------------------------------*/
+
+/*
+> MeuErro.name
+< "InvalidMessage"
+> MeuErro.stack
+< "InvalidMessage: Mensagem Inválida\n at <anonymous>:3:17"
+> MeuErro
+< InvalidMessage: Mensagem Inválida\n 
+at <anonymous>:3:17
+*/
